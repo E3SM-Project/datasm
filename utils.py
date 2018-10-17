cb651e113f71514973e7e623aa7f8208a34f6d19
Sterling Baldwin <baldwin32@llnl.gov>
2018-10-17 16:35:15 -0700

    structure_gen is working, transfer is almost there

--- utils.py
+++ utils.py
@@ -1,15 +1,15 @@
 """
 A module of utilities for automating the ESGF publciation process
 """
+from __future__ import print_function
 import os
-from pathlib import Path
-from fnmatch import fnmatch
 from subprocess import call, Popen, PIPE
 from shutil import move, copy
 from time import sleep
+from tqdm import tqdm
 
 
-def structure_gen(basepath, casename, grids, atmos_res, ocean_res, file_types, *args, **kwargs):
+def structure_gen(basepath, casename, grid, atmos_res, ocean_res, data_paths):
     """
     generate the esgf publication structure
 
@@ -19,28 +19,39 @@ def structure_gen(basepath, casename, grids, atmos_res, ocean_res, file_types, *
         grids (list(str)): any grids in addition to native that are being published
         atmos_res (str): the atmospheric resolution i.e. 1deg
         ocean_res (str): the ocean resolution i.e. 60-30km
-        file_types (dict): a dictionary with keys with the file type name, and values of the
-            glob to use to match those files
+        data_paths (dict): a dictionary with keys with the file type name, and values of the
+            path to where those files are stored
     """
 
     # make the top level directories
-    
     resolution_dir = os.path.join(
         basepath,
         casename,
         '{atm_res}_atm_{ocn_res}_ocean'.format(
             atm_res=atmos_res,
             ocn_res=ocean_res))
-    makedir(resolution_dir)
 
+    # make the list of descrete types to handle
+    dtypes = list()
+    for dtype in data_paths.keys():
+        index = dtype.find('_')
+        if index > 0:
+            new_type = dtype[:index]
+        else:
+            new_type = dtype
+        if new_type not in dtypes:
+            dtypes.append(new_type)
+
+    grids = ['native', grid]
+    new_paths = list()
     # iterate over the file types and create required subdirectories
-    for dtype in file_types.keys():
+    for dtype in dtypes:
         # /basedir/resolution_dir/dtype
         dtype_dir = os.path.join(resolution_dir, dtype)
         # all data types have a 'native' grid type
         grid_dir = os.path.join(dtype_dir, 'native')
         # /basedir/resolution_dir/dtype/native/model-output/mon/ens1/v1
-        makedir(
+        new_paths.append(
             os.path.join(
                 grid_dir,
                 'model-output',
@@ -52,15 +63,16 @@ def structure_gen(basepath, casename, grids, atmos_res, ocean_res, file_types, *
             for grid in grids:
                 grid_dir = os.path.join(dtype_dir, grid)
                 # /basedir/resolution_dir/dtype/grid/model-output/mon/ens1/v1
-                makedir(os.path.join(
-                    grid_dir,
-                    'model-output',
-                    'mon',
-                    'ens1',
-                    'v1'))
+                new_paths.append(
+                    os.path.join(
+                        grid_dir,
+                        'model-output',
+                        'mon',
+                        'ens1',
+                        'v1'))
                 if dtype == 'atmos' and grid != 'native':
                     # /basedir/resolution_dir/dtype/grid/climo/monClim/ens1/v1
-                    makedir(
+                    new_paths.append(
                         os.path.join(
                             grid_dir,
                             'climo',
@@ -68,7 +80,7 @@ def structure_gen(basepath, casename, grids, atmos_res, ocean_res, file_types, *
                             'ens1',
                             'v1'))
                     # /basedir/resolution_dir/dtype/grid/climo/seasonClim/ens1/v1
-                    makedir(
+                    new_paths.append(
                         os.path.join(
                             grid_dir,
                             'climo',
@@ -76,16 +88,19 @@ def structure_gen(basepath, casename, grids, atmos_res, ocean_res, file_types, *
                             'ens1',
                             'v1'))
                     # /basedir/resolution_dir/dtype/grid/time-series/mon/ens1/v1
-                    makedir(
+                    new_paths.append(
                         os.path.join(
                             grid_dir,
                             'time-series',
                             'mon',
                             'ens1',
                             'v1'))
-        # set the permissions so the ESGF server can open the directories
-        cmd = ['chmod', '-R', 'a+rx', os.path.join(basepath, casename)]
-        call(cmd)
+    for path in tqdm(new_paths):
+        makedir(path)
+    # set the permissions so the ESGF server can open the directories
+    cmd = ['chmod', '-R', 'a+rx', os.path.join(basepath, casename)]
+    call(cmd)
+
 
 def makedir(directory):
     """
@@ -94,48 +109,23 @@ def makedir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
-def collect_files(input_path, file_types, excludes):
-    """
-    Walks a directory tree to find files, doesnt go down any directory that matches a pattern in excludes 
-    
-    Parameters
-    ----------
-        input_path (str): root of search tree
-        file_types (dict): a map of file_type names to globs that match that type
-        excludes (list): a list of strings of directory names to avoid searching in
-    Returns
-    -------
-        a dict of lists, the keys are file_types with non-zero files that match, the list
-            is a list of paths to files of that type that were found in the tree
-    """
-    datafiles = dict()
-    exclude = set(excludes)
-    for root, dirs, files in os.walk(input_path):
-        dirs[:] = set(dirs) - exclude
-        for file in files:
-            for ftype in file_types.keys():
-                if fnmatch(file, file_types[ftype]):
-                    if not datafiles.get(ftype):
-                        datafiles[ftype] = list()
-                    path = Path(root, file).absolute()
-                    datafiles[ftype].append(path)
-                    break
-    return datafiles
 
-def transfer_files(basepath, mode, case, file_types, excludes):
+def transfer_files(outpath, case, mode, grid, data_paths):
     """
     Move or copy data into the ESGF publication structure
 
     Parameters
     ----------
-        basepath (str): the base of the ESGF publication structure
+        outpath (str): the base of the ESGF publication structure
         mode (str): either 'move' or 'copy'
-        case (str): the full name of the case
-s       file_types (list): List of tuples of (file_type(str), regex that matches files of this type(str))
+        case (str): the case being published
+        grid (str): the non-native grid name
+        data_paths (dict): a dictionary with keys with the file type name, and values of the
+            path to where those files are stored
     Returns
     -------
-        True if all data is moved/copied successfuly
-        False otherwise
+        0 if everything completed successfully
+        -1 on error
     """
     if mode not in ['copy', 'move', 'link']:
         raise Exception('{} is not a supported mode'.format(mode))
@@ -148,15 +138,35 @@ s       file_types (list): List of tuples of (file_type(str), regex that matches
 
     # the first subdirectory is a directory with the name
     # of the atm resolution and the ocean resolution
-    resolution_dir = os.listdir(os.listdir(basepath)[0])[0]
+    resolution_dir = os.listdir(
+        os.path.join(outpath, 
+            os.listdir(outpath)[0]))[0]
+    if not resolution_dir:
+        raise Exception('Missing resolution directory')
 
-    """
-    walk over the raw input file directory and the output/pp directories
-    collecting all the files we're meant to publish, then transfer them to
-    the destination
-    """
-    files = collect_files(basepath, file_types, excludes)
-
+    for dtype, path in data_paths.items():
+        contents = os.listdir(path)
+        for item in tqdm(contents, desc=dtype):
+            src = os.path.join(path, item)
+            dst = _setup_dst(
+                case=case,
+                basepath=outpath,
+                res_dir=resolution_dir,
+                grid=grid,
+                datatype=dtype,
+                filename=item)
+            if os.path.exists(dst):
+                continue
+            if not os.path.exists(src):
+                print_message('{} does not exist'.format(src))
+                return -1
+            try:
+                transfer(src, dst)
+            except OSError as error:
+                print(src, dst)
+                print(repr(error))
+                return -1
+    return 0
 
 def mapfile_gen(basepath, inipath, casename, maxprocesses, event=None):
     """
@@ -176,8 +186,8 @@ def mapfile_gen(basepath, inipath, casename, maxprocesses, event=None):
            '--outdir', outpath,
            '-i', inipath,
            '--project', 'e3sm',
-           '--max-processes', maxprocesses,
-           'datapath']
+           '--max-processes', str(maxprocesses),
+           datapath]
     proc = Popen(cmd, stdout=PIPE, stderr=PIPE)
     while proc.poll() is None:
         if event is not None and event.is_set():
@@ -185,37 +195,41 @@ def mapfile_gen(basepath, inipath, casename, maxprocesses, event=None):
         sleep(1)
 
 
-def _setup_dst(short_name, basepath, res_dir, grid, datatype, filename):
-    """"""
+def _setup_dst(case, basepath, res_dir, grid, datatype, filename):
+    """
+    Find the destination path for a file
+    """
     freq = 'mon'
-    if datatype in ['atm', 'atm_regrid', 'atm_ts_regrid', 'climo_regrid']:
+    if datatype in ['atmos', 'atmos_regrid', 'atmos_ts', 'atmos_climo']:
         type_dir = 'atmos'
-        if datatype == 'atm':
+        if datatype == 'atmos':
             output_type = 'model-output'
-        elif datatype == 'atm_ts_regrid':
+        elif datatype == 'atmos_ts':
             output_type = 'time-series'
-        elif datatype == 'climo_regrid':
+        elif datatype == 'atmos_climo':
             output_type = 'climo'
             freq = 'monClim'
             for season in ['ANN', 'DJF', 'MAM', 'JJA', 'SON']:
                 if season in filename:
                     freq = 'seasonClim'
                     break
-    elif datatype in ['lnd', 'lnd_regrid']:
+    elif datatype in ['land', 'land_regrid']:
         type_dir = 'land'
         output_type = 'model-output'
-    elif datatype == 'ocn':
+    elif datatype == 'ocean':
         type_dir = 'ocean'
         output_type = 'model-output'
-    elif datatype == 'ice':
+        grid = 'native'
+    elif datatype == 'sea-ice':
         type_dir = 'sea-ice'
         output_type = 'model-output'
+        grid = 'native'
     else:
         raise Exception('{} is an invalid data type'.format(datatype))
 
     return os.path.join(
         basepath,
-        short_name,
+        case,
         res_dir,
         type_dir,
         grid,
@@ -224,3 +238,25 @@ def _setup_dst(short_name, basepath, res_dir, grid, datatype, filename):
         'ens1',
         'v1',
         filename)
+
+class colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def print_message(message, status='error'):
+    """
+    Prints a message with either a green + or a red -
+
+    Parameters:
+        message (str): the message to print
+        status (str): th"""
+    if status == 'error':
+        print(colors.FAIL + '[-] ' + colors.ENDC + colors.BOLD + str(message) + colors.ENDC)
+    elif status == 'ok':
+        print(colors.OKGREEN + '[+] ' + colors.ENDC + str(message))
