b93ccb504896ff19003b2b1892913e68f96ebf90
Sterling Baldwin <baldwin32@llnl.gov>
2018-10-18 12:12:27 -0700

    Another attempt at pip installing esgprep

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,1 +1,2 @@
-python setup.py install && pip install -e git://github.com/ESGF/esgf-prepare.git@master#egg=esgprep
+python setup.py install
+pip install -e git://github.com/ESGF/esgf-prepare.git@master#egg=esgprep
