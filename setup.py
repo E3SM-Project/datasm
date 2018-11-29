4081b6a43f4a19a87f1989c1515f4b7bf7caabdc
Baldwin <baldwin32@ml-9635195.llnl.gov>
2018-11-29 09:23:33 -0800

    Consolidates code into a single script

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="esgfpub",
-    version="0.0.2",
+    version="0.0.4",
     author="Sterling Baldwin",
     author_email="baldwin32@llnl.gov",
     description="E3SM Automated publication to ESGF",
