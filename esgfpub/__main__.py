01fb0feac1547004a6298b6781d868439c71afad
Sterling Baldwin <baldwin32@llnl.gov>
2020-03-10 11:36:37 -0700

    Splits out all the checker code into its own file

--- esgfpub/__main__.py
+++ esgfpub/__main__.py
@@ -1,132 +1,11 @@
 """
 A tool for automating much of the ESGF publication process
 """
-
-from esgfpub.util import transfer_files, mapfile_gen, validate_raw, makedir, parse_args
-from esgfpub.util import print_message
-from esgfpub.publication_checker import publication_checker
-import sys
-import os
-from threading import Event
-from configobj import ConfigObj
-from tqdm import tqdm
-
-
-def check(ARGS):
-    return publication_checker(
-        spec_path=ARGS.case_spec,
-        data_path=ARGS.data_path,
-        cases=ARGS.cases,
-        ens=ARGS.ens,
-        tables=ARGS.tables,
-        variables=ARGS.variables,
-        published=ARGS.published,
-        sproket=ARGS.sproket,
-        max_connections=ARGS.max_connections,
-        serial=ARGS.serial,
-        debug=ARGS.debug,
-        dataset_ids=ARGS.dataset_ids,
-        projects=ARGS.project,
-        to_json=ARGS.to_json)
-
-
-def publish(ARGS):
-    if not ARGS.config:
-        PARSER.print_help()
-        return 1
-
-    if ARGS.over_write:
-        overwrite = True
-    else:
-        overwrite = False
-
-    try:
-        CONFIG = ConfigObj(ARGS.config)
-    except SyntaxError as error:
-        print_message("Unable to parse config file")
-        print(repr(error))
-        return 1
-
-    try:
-        BASEOUTPUT = CONFIG['output_path']
-        ATMRES = CONFIG['atmospheric_resolution']
-        OCNRES = CONFIG['ocean_resolution']
-        DATA_PATHS = CONFIG['data_paths']
-        ENSEMBLE = CONFIG['ensemble']
-        EXPERIMENT_NAME = CONFIG['experiment']
-        GRID = CONFIG.get('non_native_grid')
-        START = int(CONFIG['start_year'])
-        END = int(CONFIG['end_year'])
-    except ValueError as error:
-        print_message('Unable to find values in config file')
-        print(repr(error))
-        return 1
-
-    print_message('Validating raw data', 'ok')
-    if not validate_raw(DATA_PATHS, START, END):
-        return 1
-
-    resdirname = "{}_atm_{}_ocean".format(ATMRES, OCNRES)
-    makedir(os.path.join(BASEOUTPUT, EXPERIMENT_NAME, resdirname))
-
-    transfer_mode = ARGS.transfer_mode
-    if transfer_mode == 'move':
-        print_message('Moving files', 'ok')
-    elif transfer_mode == 'copy':
-        print_message('Copying files', 'ok')
-    elif transfer_mode == 'link':
-        print_message('Linking files', 'ok')
-    num_moved = transfer_files(
-        outpath=BASEOUTPUT,
-        experiment=EXPERIMENT_NAME,
-        grid=GRID,
-        mode=transfer_mode,
-        data_paths=DATA_PATHS,
-        ensemble=ENSEMBLE,
-        overwrite=overwrite)
-    if num_moved == -1:
-        return 1
-
-    RUNMAPS = CONFIG.get('mapfiles', False)
-    if not RUNMAPS or RUNMAPS not in [True, 'true', 'True', 1, '1']:
-        print_message('Not running mapfile generation', 'ok')
-        print_message('Publication prep complete', 'ok')
-        return 0
-    else:
-        print_message('Starting mapfile generation', 'ok')
-
-    try:
-        INIPATH = CONFIG['ini_path']
-        MAPOUT = ARGS.mapout
-    except:
-        raise ValueError(
-            "Mapfiles generation is turned on, but the config is missing the ini_path option")
-    NUMWORKERS = CONFIG.get('num_workers', 4)
-    event = Event()
-
-    pbar = tqdm(
-        desc="Generating mapfiles",
-        total=num_moved)
-    try:
-        res = mapfile_gen(
-            basepath=BASEOUTPUT,
-            inipath=INIPATH,
-            experiment=EXPERIMENT_NAME,
-            outpath=MAPOUT,
-            maxprocesses=NUMWORKERS,
-            event=event,
-            pbar=pbar)
-    except KeyboardInterrupt as error:
-        print_message('Keyboard interrupt ... exiting')
-        event.set()
-        return 1
-    else:
-        if res == 0:
-            print_message('Publication prep complete', 'ok')
-        else:
-            print_message(
-                'mapfile generation exited with status: {}'.format(res), 'error')
-        return res
+from sys import exit as sysexit
+from esgfpub.checker import data_check
+from esgfpub.publisher import publish
+from esgfpub.stager import stage
+from esgfpub.util import parse_args
 
 
 def main():
@@ -134,12 +13,37 @@ def main():
     ARGS = parse_args()
     subcommand = ARGS.subparser_name
     if subcommand == 'check':
-        return check(ARGS)
+        return data_check(
+            spec_path=ARGS.case_spec,
+            data_path=ARGS.data_path,
+            cases=ARGS.cases,
+            ens=ARGS.ens,
+            tables=ARGS.tables,
+            variables=ARGS.variables,
+            published=ARGS.published,
+            verify=ARGS.verify,
+            model_versions=ARGS.model_versions,
+            sproket=ARGS.sproket,
+            max_connections=ARGS.max_connections,
+            serial=ARGS.serial,
+            debug=ARGS.debug,
+            dataset_ids=ARGS.dataset_ids,
+            projects=ARGS.project,
+            to_json=ARGS.to_json)
+    elif subcommand == 'stage':
+        return stage(ARGS)
     elif subcommand == 'publish':
-        return publish(ARGS)
+        return publish(
+            mapsin=ARGS.maps_in, 
+            mapsout=ARGS.maps_done, 
+            mapserr=ARGS.maps_err,
+            ini=ARGS.ini,
+            loop=ARGS.loop,
+            username=ARGS.username,
+            debug=ARGS.debug)
     else:
         raise ValueError("Unrecognised subcommand")
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sysexit(main())
---
+++ esgfpub/publisher.py
@@ -0,0 +1,107 @@
+import os
+import stat
+import pexpect
+import getpass
+from os import remove
+from time import sleep
+from subprocess import Popen, PIPE, CalledProcessError
+from tempfile import NamedTemporaryFile
+from esgfpub.util import print_message
+
+def execute(cmd):
+    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    while proc.poll() is None:
+        line = proc.stdout.readline()
+        if line != "":
+            print_message(line, 'ok')
+ 
+        line = proc.stderr.readline()
+        if line != "" and 'psycopg2' not in line and ' """)' not in line:
+            print_message(line)
+    if proc.returncode:
+        raise CalledProcessError(proc.returncode)
+        
+
+
+def publish_maps(mapfiles, ini, mapsin, mapsout, mapserr, username, password, debug=False):
+    for m in mapfiles:
+        if debug:
+            print_message('Starting mapfile: {}'.format(m), 'info')
+        if m[-4:] != '.map':
+            continue
+        if m[:5] == 'CMIP6':
+            project = 'cmip6'
+        elif m[:4] == 'E3SM':
+            project = 'e3sm'
+        else:
+            raise ValueError(
+                "Unrecognized project name for mapfile: {}".format(m))
+        
+        if debug:
+            print_message('Creating myproxy certificate', 'info')
+        cmd = 'myproxy-logon -s esgf-node.llnl.gov -l {} -t 72 -o ~/.globus/certificate-file'.format(username)
+        if debug:
+            print_message(cmd, 'info')
+        proc = pexpect.spawn(cmd)
+        proc.expect('Enter MyProxy pass phrase:')
+        proc.sendline(password)
+        
+        script = """#!/bin/sh
+source /usr/local/conda/bin/activate esgf-pub
+esgpublish -i {ini} --project {project} --map {map} --commit-every 100 --no-thredds-reinit
+esgpublish -i {ini} --project {project} --map {map} --service fileservice --noscan --thredds  --no-thredds-reinit
+esgpublish --project {project} --thredds-reinit
+esgpublish -i {ini} --project {project} --map {map} --service fileservice --noscan --publish
+""".format(
+            ini=ini, map=os.path.join(mapsin, m), project=project, username=username, password=password)
+
+        tempfile = NamedTemporaryFile(delete=False)
+
+        import ipdb; ipdb.set_trace()
+        with open(tempfile.name, 'w') as fp:
+            fp.write(script)
+        st = os.stat(tempfile.name)
+        os.chmod(tempfile.name, st.st_mode | stat.S_IEXEC)
+        
+        if debug:
+            print_message('Running publication script: {}'.format(tempfile.name), 'info')
+            print_message(script, 'info')        
+
+        
+        try:
+            cmd = ['bash', tempfile.name]
+            try:
+                execute(cmd)
+            except Exception as e:
+                print_message("Error in publication, moving {} to {}".format(m, mapserr))
+                os.rename(
+                    os.path.join(mapsin, m),
+                    os.path.join(mapserr, m))
+                raise(e)
+            else:
+                if debug:
+                    print_message("Publication success, moving {} to ".format(m, mapsout), "info")
+                os.rename(
+                    os.path.join(mapsin, m),
+                    os.path.join(mapsout, m))
+        finally:
+            if not fp.closed:
+                fp.close()
+
+def publish(mapsin, mapsout, mapserr, ini, loop, username, debug=False):
+
+    # password = getpass.getpass("Please enter my-proxy logon password: ")
+    password = 'Ab1-B4sCkxaW'
+    if loop:
+        if debug:
+            print_message("Entering publication loop", "info")
+        while True:
+            mapfiles = os.listdir(mapsin)
+            if not mapfiles:
+                sleep(30)
+            else:
+                publish_maps(mapfiles, ini, mapsin, mapsout, mapserr, username, password, debug)
+    else:
+        mapfiles = os.listdir(mapsin)
+        publish_maps(mapfiles, ini, mapsin, mapsout, mapserr, username, password, debug)
+    return 0
