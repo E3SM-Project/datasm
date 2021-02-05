1680605b8bb9c2118a3c7a4270e7aca9659d1682
Sterling Baldwin <baldwin32@llnl.gov>
2021-02-05 15:32:36 -0800

    Fleshes out the FixTimeUnits job

--- warehouse/scripts/fix_time_units.py
+++ warehouse/scripts/fix_time_units.py
@@ -1,65 +1,92 @@
 import os
+from sys import exit
 import argparse
 from tqdm import tqdm
-import xarray as xr
-from subprocess import Popen, PIPE
+from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('input', 
-                        help="path to directory containing data with incorrect time units")
-    parser.add_argument('output', 
-                        help="path to directory where corrected data should be saved")
-    parser.add_argument('--bounds-name', 
-                        type=str, default="time_bnds")
-    parser.add_argument('--time-units',
-                        default="days since 1850-01-01 00:00:00")
-    parser.add_argument('--processes', 
-                        default=6, type=int)
-    parser.add_argument('--quiet', 
-                        action="store_true", help="Suppress progressbars")
-    return parser.parse_args()
-
-
-def fix_units(inpath, outpath, time_units, bounds_name):
-
+def fix_units(inpath, outpath, time_units, offset):
+    import xarray as xr
     with xr.open_dataset(inpath, decode_times=False) as ds:
+        if ds.get('time') is None:
+            raise ValueError(f"{inpath} has no 'time' axis")
+        bnds_name = 'time_bnds' if ds.get(
+            'time_bnds') is not None else 'time_bounds'
         if ds['time'].attrs.get('units') != time_units:
+            ds = ds.assign_coords(time=ds['time']+offset)
+            if bnds_name == 'time_bnds':
+                ds = ds.assign_coords(time_bnds=ds[bnds_name]+offset)
+            else:
+                ds = ds.assign_coords(time_bounds=ds[bnds_name]+offset)
+            if ds[bnds_name].values[0][0] == ds[bnds_name].values[0][1]:
+                freq = ds[bnds_name].values[1][1] - ds[bnds_name].values[1][0]
+                ds[bnds_name].values[0][0] -= freq
             ds['time'].attrs = {
                 'long_name': "time",
                 'units': time_units,
                 'calendar': "noleap",
-                'bounds': bounds_name }
+                'bounds': bnds_name
+            }
+            ds['time_bnds'].attrs = {
+                "long_name": "time interval endpoints"
+            }
             ds.to_netcdf(outpath, unlimited_dims=['time'])
+        else:
+            os.symlink(inpath, outpath)
 
 
 def main():
-    
-    parsed_args = parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'input',
+        help="path to directory containing data with incorrect time units")
+    parser.add_argument(
+        'output',
+        help="path to directory where corrected data should be saved")
+    parser.add_argument(
+        '-t', '--time-offset',
+        type=float, default=0.0,
+        help="Value to increase time and time_bnds index by")
+    parser.add_argument(
+        '-d', '--time-units',
+        default="days since 1850-01-01 00:00:00")
+    parser.add_argument(
+        '-p', '--processes',
+        default=6, type=int,
+        help="Number of parallel processes")
+    parser.add_argument(
+        '-q', '--quiet',
+        action="store_true",
+        help="Suppress progress bars")
+    args = parser.parse_args()
+    os.makedirs(args.output, exist_ok=True)
 
-    os.makedirs(parsed_args.output, exist_ok=True)
-
-    with ProcessPoolExecutor(max_workers=parsed_args.processes) as pool:
-
-        files = os.listdir(parsed_args.input)
+    total = 0
+    for _ in Path(args.input).glob('*'):
+        total += 1 
+    with ProcessPoolExecutor(max_workers=args.processes) as pool:
         futures = []
-
-        for f in files:
-            inpath = os.path.join(parsed_args.input, f)
-            outpath = os.path.join(parsed_args.output, f)
-            futures.append(
-                pool.submit(
-                    fix_units,
+        for path in Path(args.input).glob('*'):
+            inpath = str(path.resolve())
+            outpath = str(Path(args.output, path.name).resolve())
+            if args.processes > 1:
+                futures.append(
+                    pool.submit(
+                        fix_units,
+                        inpath,
+                        outpath,
+                        args.time_units,
+                        args.time_offset))
+            else:
+                fix_units(
                     inpath,
                     outpath,
-                    parsed_args.time_units,
-                    parsed_args.time_offset))
-
-        for _ in tqdm(as_completed(futures), total=len(files), disable=parsed_args.quiet):
-            pass
-
+                    args.time_units,
+                    args.time_offset)
+        if args.processes > 1:
+            for _ in tqdm(as_completed(futures), total=total, disable=args.quiet):
+                pass
     return 0
 
 
