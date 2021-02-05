af8fc6a429d19438fea03dc9bbd67bfcc3d3e28e
Sterling Baldwin <baldwin32@llnl.gov>
2021-02-05 15:33:22 -0800

    Gets the next_state method working across nested workflows

--- warehouse/workflows/__init__.py
+++ warehouse/workflows/__init__.py
@@ -45,7 +45,7 @@ class Workflow(object):
         else:
             return prefix
 
-    def next_state(self, dataset, status, idx=0):
+    def next_state(self, dataset, state, params, idx=0):
         """
         Parameters: 
             dataset (Dataset) : The dataset which is changing state
@@ -53,45 +53,47 @@ class Workflow(object):
             idx (int) : The recursive depth index
         Returns the name of the next state to transition to given the current state of the dataset
         """
-
-        status_attrs = status.split(':')
-        if len(status_attrs) < 3:
-            target_state = status
+        state_attrs = state.split(':')
+        if len(state_attrs) < 3:
+            target_state = state
         else:
-            target_state = f"{status_attrs[-3]}:{status_attrs[-2]}"
+            target_state = f"{state_attrs[-3]}:{state_attrs[-2]}"
         prefix = self.get_status_prefix()
         if target_state in self.transitions.keys():
             target_data_type = f'{dataset.realm}-{dataset.grid}-{dataset.freq}'
             transitions = self.transitions[target_state].get(target_data_type)
             if transitions is None:
-                return [(x, self) for x in self.transitions[target_state]['default']]
+                return [(f'{prefix}{x}:', self, params) for x in self.transitions[target_state]['default']]
             else:
-                return [(f'{prefix}{t}:', self) for t in transitions]
+                return [(f'{prefix}{x}:', self, params) for x in transitions]
             
-        elif status_attrs[idx] == "WAREHOUSE":
-            return self.next_state(dataset, status, idx + 1)
+        elif state_attrs[idx] == "WAREHOUSE":
+            return self.next_state(dataset, state, params, idx + 1)
 
-        elif status_attrs[idx] in self.children.keys():
-            return self.children[status_attrs[idx]].next_state(dataset, status, idx + 1)
+        elif state_attrs[idx] in self.children.keys():
+            return self.children[state_attrs[idx]].next_state(dataset, state, params, idx + 1)
 
         else:
-            if "Exit:Success" in target_state:
-                dataset.update_status(f'{prefix}Pass:')
-                return None
-            elif "Exit:Fail" in target_state:
-                dataset.update_status(f'{prefix}Fail:')
-                return None
-            else:
-                raise ValueError(
-                    f"{target_state} is not present in the transition graph for {self.name}")
+            raise ValueError(
+                f"{target_state} is not present in the transition graph for {self.name}")
 
-    def get_job(self, dataset, state, scripts_path, slurm_out_path, workflow, **kwargs):
+    def get_job(self, dataset, state, params, scripts_path, slurm_out_path, workflow, job_workers=8, **kwargs):
 
-        job = self.jobs[state]
+        state_attrs = state.split(':')
+        job_name = state_attrs[-3]
+
+        parent = f"{state_attrs[0]}:{state_attrs[1]}"
+    
+        job = self.jobs[job_name]
         job_instance = job(
-            dataset, state, scripts_path, slurm_out_path, 
+            dataset, 
+            state, 
+            scripts_path, 
+            slurm_out_path,
+            params=params,
             slurm_opts=kwargs.get('slurm_opts', []), 
-            parent=workflow.get_status_prefix())
+            parent=parent,
+            job_workers=job_workers)
         job_instance.setup_requisites()
         return job_instance
         
