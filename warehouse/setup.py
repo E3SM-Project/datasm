b01b31523d8f7a5bbd155eadbe4f5c3c5517f1ce
Sterling Baldwin <baldwin32@llnl.gov>
2021-02-22 12:20:27 -0800

    Adds a "clean" command to the setup

--- warehouse/setup.py
+++ warehouse/setup.py
@@ -4,6 +4,34 @@ Setup for the E3SM warehouse
 from setuptools import find_packages, setup
 from warehouse.version import __version__
 
+import distutils.cmd
+import os
+
+class CleanCommand(distutils.cmd.Command):
+    """
+    Our custom command to clean out junk files.
+    """
+    description = "Cleans out junk files we don't want in the repo"
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        cmd_list = dict(
+            DS_Store="find . -name .DS_Store -print0 | xargs -0 rm -f;",
+            pyc="find . -name '*.pyc' -exec rm -rf {} \;",
+            empty_dirs="find ./pages/ -type d -empty -delete;",
+            build_dirs="find . -name build -print0 | xargs -0 rm -rf;",
+            dist_dirs="find . -name dist -print0 | xargs -0 rm -rf;",
+            egg_dirs="find . -name *.egg-info -print0 | xargs -0 rm -rf;"
+        )
+        for key, cmd in cmd_list.items():
+            os.system(cmd)
+
 setup(
     name="warehouse",
     version=__version__,
@@ -15,4 +43,7 @@ setup(
             ['warehouse = warehouse.__main__:main' ]},
     packages=find_packages(),
     package_dir={'warehouse': 'warehouse'},
-    include_package_data=True)
+    include_package_data=True,
+    cmdclass={
+        'clean': CleanCommand,
+    })
