d9357fcc717142c5f7254267b7c1cb2c8443f72c
Sterling Baldwin <baldwin32@llnl.gov>
2021-03-10 13:57:58 -0800

    Cleanup of the mapfile validator script

--- warehouse/warehouse/scripts/validate_mapfile.py
+++ warehouse/warehouse/scripts/validate_mapfile.py
@@ -1,77 +1,70 @@
-import sys, os
+import sys
+import os
 import argparse
-from argparse import RawTextHelpFormatter
-import glob
 import shutil
 import subprocess
 import time
+from pathlib import Path
 from datetime import datetime
 
-subcommand = ''
-gv_logname = ''
+# gv_logname = ''
 
 '''
     Usage:  validate_mapfile --data-path version_path --mapfile mapfile_path
 
     Returns 0 if every file in version path is listed in the mapfile
-
 '''
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description="Ensure every datafile in supplied data_path exists in the given mapfile.")
-    parser.add_argument('--data-path', type=str, dest='datapath', required=True, help="source directory of netCDF files to seek in mapfile")
-    parser.add_argument('--mapfile', type=str, dest='mapfile', required=True, help="mapfile to be validated")
+        description="Ensure every datafile in supplied data-path exists in the given mapfile.")
+    parser.add_argument(
+        '--data-path', 
+        type=str, 
+        dest='datapath', 
+        required=True, 
+        help="source directory of netCDF files to seek in mapfile")
+    parser.add_argument(
+        '--mapfile', 
+        type=str, 
+        required=True, 
+        help="mapfile to be validated")
     return parser.parse_args()
 
 
-# ======== convenience ========================
-
-def ts(prefix):
-    return prefix + datetime.now().strftime('%Y%m%d_%H%M%S')
-
-def loadFileLines(afile):
+def loadFileLines(file: str):
     retlist = []
-    if len(afile):
-        with open(afile,"r") as f:
-            retlist = f.read().split('\n')
-        retlist = [ _ for _ in retlist if _[:-1] ]
+    filepath = Path(file)
+    if not filepath.exists():
+        raise ValueError(f"Cannot load lines from file {filepath} as it does not exist")
+    
+    with open(filepath.resolve(), "r") as instream:
+        retlist = [x for x in instream.read().split('\n') if x[:-1]]
     return retlist
 
-def logMessageInit(logtitle):
-    global gv_logname
-    gv_logname = f'{logtitle}-{ts("")}'
-    open(gv_logname, 'a').close()
 
-def logMessage(mtype,message):
-    outmessage = f'{ts("TS_")}:{mtype}:{message}\n'
-    with open(gv_logname, 'a') as f:
-        f.write(outmessage)
+def validate_mapfile(mapfile: str, srcdir: Path):
+    ''' 
+    at this point, the srcdir should contain the datafiles (*.nc)
+    and the parent dir/.mapfile, so we can do a name-by-name comparison.
+    MUST test for each srcdir datafile in mapfile listing.
 
-# ======== warehouse ========================
-
-do_logging = False
-
-def validate_mapfile(mapfile,srcdir):
-    ''' at this point, the srcdir should contain the datafiles (*.nc)
-        and the parent dir/.mapfile, so we can do a name-by-name comparison.
-        MUST test for each srcdir datafile in mapfile listing.
+    Params:
+        mapfile (str): the string path to the mapfile
+        srcdir (Path): a Path object pointint to the directory containing the data files
+    Returns:
+        True if the mapfile is valid, False otherwise
     '''
-    dataset_files = sorted(glob.glob(srcdir + '/*.nc'))
+    dataset_files = sorted([x for x in srcdir.glob('*.nc')])
     mapfile_lines = sorted(loadFileLines(mapfile))
 
     if not len(dataset_files) == len(mapfile_lines):
-        if do_logging:
-            logMessage('ERROR',f'non-matching count of files and mapfile lines: {srcdir}')
-        return False
+        raise ValueError("Number of files does not match number of entries in the mapfile")
         
     # MUST assume both lists sort identically - O(n) > O(n^2)
-    pairlist = list(zip(dataset_files,mapfile_lines))
-    for atup in pairlist:
-        if not atup[0] in atup[1]: 
-            if do_logging:
-                logMessage('ERROR',f'dataset file not listed in mapfile: {mapfile}')
-                logMessage('ERROR',f'{atup[0]} not in {atup[1]}')
+    pairlist = list(zip(dataset_files, mapfile_lines))
+    for file, mapentry in pairlist:
+        if file not in mapentry: 
             return False
 
     return True
@@ -79,19 +72,12 @@ def validate_mapfile(mapfile,srcdir):
 
 def main():
 
-    args = parse_args()
+    parsed_args = parse_args()
 
-    if do_logging:
-        logMessageInit('rlog_validate_mapfile')
-
-    success = validate_mapfile(args.mapfile,args.datapath)
-    if do_logging:
-        if success:
-            logMessage('STATUS',f'VALIDATE_MAPFILE:Pass')
-        else:
-            logMessage('STATUS',f'VALIDATE_MAPFILE:Fail:Bad_mapfile')
-
-    sys.exit(success)
+    if validate_mapfile(parsed_args.mapfile, parsed_args.datapath):
+        return 0
+    else:
+        return 1
  
 if __name__ == "__main__":
   sys.exit(main())
