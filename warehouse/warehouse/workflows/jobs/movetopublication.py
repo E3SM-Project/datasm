0bf4e36be03332fc441a085ee23310b38480e4be
Tony Bartoletti <bartoletti1@llnl.gov>
2021-03-11 19:20:33 -0800

    accommodate latest corresponding script

--- warehouse/warehouse/workflows/jobs/movetopublication.py
+++ warehouse/warehouse/workflows/jobs/movetopublication.py
@@ -7,4 +7,8 @@ class MoveToPublication(WorkflowJob):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = NAME
-        self.cmd = ''
+        self._cmd = f"""
+cd {self.scripts_path}
+python move_to_publication.py --src-path {self.dataset.latest_warehouse_dir} --dst-path {self.dataset.latest_publication_dir}
+"""
+
