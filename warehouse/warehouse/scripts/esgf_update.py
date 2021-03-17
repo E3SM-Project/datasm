434a0b03300d52151cdfe18d1c7ae8ad1fa14431
Sterling Baldwin <baldwin32@llnl.gov>
2021-03-17 15:31:31 -0700

    Adds a script to update arbitrary ESGF facet values

---
+++ warehouse/warehouse/scripts/esgf_update.py
@@ -0,0 +1,114 @@
+from esgcet.pub_client import publisherClient
+import sys, json, requests
+import argparse
+import configparser as cfg
+from datetime import datetime
+from pathlib import Path
+
+
+DEFAULT_INDEX_NODE = "esgf-node.llnl.gov"
+DEFAULT_DATA__NODE = "esgf-data2.llnl.gov"
+
+def gen_xml(dataset_id, datatype, facets):
+    now = datetime.utcnow()
+    ts = now.strftime("%Y-%m-%dT%H:%M:%SZ")
+    idfield = "id"
+    if datatype == "files":
+        idfield = "dataset_id"
+    txt = f"""<updates core="{datatype}" action="set">
+        <update>
+          <query>{idfield}={dataset_id}</query>
+          """
+
+    for key, value in facets.items():
+        txt += """<field name="{key}">
+                       <value>{value}</value>
+                   </field>
+        """
+
+    txt += f"""
+          <field name="_timestamp">
+             <value>{ts}</value>
+          </field>
+        </update>
+    </updates>"""
+
+    return txt
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f', '--facets',
+        required=True,
+        nargs="*",
+        help="Space sepparated key/value pairs for facets to get updated, in the form key=value")
+    parser.add_argument(
+        '-c', '--cert',
+        required=True,
+        help="Path to ESGF cert")
+    parser.add_argument(
+        '-d', '--dataset-id',
+        required=True,
+        help="Dataset-id to be updated, this should be the whole dataset_id with the version number at the end")
+    parser.add_argument(
+        '--index-node',
+        default=DEFAULT_INDEX_NODE,
+        help=f"Path to ESGF cert, default={DEFAULT_INDEX_NODE}")
+    parser.add_argument(
+        '--data-node',
+        default=DEFAULT_DATA__NODE,
+        help=f"Path to ESGF cert, default={DEFAULT_DATA__NODE}")
+    parser.add_argument(
+        '--verbose',
+        action="store_true",
+        help="Print more verbose status messages")
+
+    args = parser.parse_args()
+
+    cert_path = args.cert
+    dataset_id = args.dataset_id
+    # trim off the version number to get the master id
+    master_id = '.'.join(dataset_id.split('.')[:-1])
+    index_node = args.index_node
+    data_node = args.data_node
+    verbose = args.verbose
+    
+    facets = {}
+    for item in args.facets:
+        key, value = item.split('=')
+        facets[key] = value
+
+    url = f'https://{index_node}/esg-search/search/?latest=true&distrib=false&format=application%2Fsolr%2Bjson&data_node={data_node}&master_id={master_id}&fields=version,id'
+    if verbose:
+        print(url)
+    res = requests.get(url)
+
+    if verbose:
+        print(res.text)
+    if not res.status_code == 200:
+        print('Error', file=sys.stderr)
+        return 1
+
+    res = json.loads(res.text)
+
+    if res['response']['numFound'] > 0:
+        docs = res['response']["docs"]
+        dataset_id = docs[0]['id']
+        # update_rec = gen_hide_xml(dsetid, "datasets")
+        client = publisherClient(cert_path, index_node)
+
+        update_record = gen_xml(dataset_id, "datasets", facets)
+        if verbose:
+            print(update_record)
+        client.update(update_record)
+        update_record = gen_xml(dataset_id, "files", facets)
+        if verbose:
+            print(update_record)
+        client.update(update_record)
+    
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
