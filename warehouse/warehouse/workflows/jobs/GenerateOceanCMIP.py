38ba5001d0be4f56dd7e80d781480a08cd1b923e
Sterling Baldwin <baldwin32@llnl.gov>
2021-04-30 12:28:36 -0700

    postprocessing transitions update

--- warehouse/warehouse/workflows/jobs/GenerateOceanCMIP.py
+++ warehouse/warehouse/workflows/jobs/GenerateOceanCMIP.py
@@ -1,11 +1,14 @@
 from warehouse.workflows.jobs import WorkflowJob
 
-NAME = 'GenerateOcnCMIP'
+NAME = 'GenerateOceanCMIP'
 
-class GenerateOcnCMIP(WorkflowJob):
+class GenerateOceanCMIP(WorkflowJob):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.name = NAME
-        self._requires = { 'ocean-native-mon': None }
+        self._requires = { 
+            'ocean-native-mon': None,
+            'atmos-native-mon': None
+        }
         self._cmd = ''
