8e3a201626be3807c701239ca356cff83d6686f1
Sterling Baldwin <baldwin32@llnl.gov>
2021-07-23 10:25:23 -0700

    Implements the ValidateEsgf script to use the dataset status checker

--- warehouse/warehouse/scripts/validate_esgf.py
+++ warehouse/warehouse/scripts/validate_esgf.py
@@ -1,12 +1,8 @@
 import sys
-import os
 import argparse
-import json
-from pathlib import Path
-from subprocess import Popen
-from tempfile import TemporaryDirectory
-from warehouse.util import sproket_with_id
-from warehouse.util import con_message
+from pprint import pprint
+from warehouse import Dataset
+from warehouse.dataset import DatasetStatus
 
 
 def parse_args():
@@ -20,23 +16,20 @@ def parse_args():
     return parser.parse_args()
 
 
-def check_dataset_published(dataset_id: str):
-    """
-    Tests whether a dataset is already available on ESGF.
-
-    Returns 0 if already published, 1 if not.
-    """
-
-    _, files = sproket_with_id(dataset_id)
-    if files is None or not files:
-        con_message("warning", f"Dataset {dataset_id} has not been published to ESGF")
-        return 1
-    return 0
-
-
 def main():
     parsed_args = parse_args()
-    return check_dataset_published(parsed_args)
+    dataset = Dataset(
+        dataset_id=parsed_args.dataset_id,
+        no_status_file=True)
+    status = dataset.get_esgf_status()
+    if status != DatasetStatus.SUCCESS.value:
+        print(f"ESGF validation failed, dataset in state {status}")
+        if missing := dataset.missing:
+            pprint(missing)
+        return 1
+    else:
+        print("ESGF validation success")
+        return 0
 
 
 if __name__ == "__main__":
