62b3c5a9d7f700633b3951a90b9647bd3de72efc
Sterling Baldwin <baldwin32@llnl.gov>
2021-08-06 10:39:54 -0700

    Improves handling of CMIP cases

--- warehouse/warehouse/workflows/jobs/PublishESGF.py
+++ warehouse/warehouse/workflows/jobs/PublishESGF.py
@@ -14,21 +14,20 @@ class PublishEsgf(WorkflowJob):
         mapfile_path = sorted([x for x in self.dataset.publication_path.glob('*.map')]).pop()
 
         optional_facets = {}
-        if 'CMIP6' not in self.dataset.dataset_id:
+        if self.dataset.project == 'E3SM':
             dataset_attrs = self.dataset.dataset_id.split('.')
             model_version = dataset_attrs[1]
             experiment_name = dataset_attrs[2]
-            with open(self._spec_path, 'r') as instream:
-                spec = yaml.load(instream, Loader=yaml.SafeLoader)
-                experiment_info = spec['project']['E3SM'][model_version][experiment_name]
-                if (campaign := experiment_info.get('campaign')):
-                    optional_facets['campaign'] = campaign
-                if (science_driver := experiment_info.get('science_driver')):
-                    optional_facets['science_driver'] = science_driver
-                if (period := experiment_info.get('period')):
-                    optional_facets['period'] = period
-                else:
-                    optional_facets['period'] = f"{experiment_info['start']} - {experiment_info['end']}"
+
+            experiment_info = self._spec['project']['E3SM'][model_version][experiment_name]
+            if (campaign := experiment_info.get('campaign')):
+                optional_facets['campaign'] = campaign
+            if (science_driver := experiment_info.get('science_driver')):
+                optional_facets['science_driver'] = science_driver
+            if (period := experiment_info.get('period')):
+                optional_facets['period'] = period
+            else:
+                optional_facets['period'] = f"{experiment_info['start']} - {experiment_info['end']}"
 
         self._cmd = f"""
             cd {self.scripts_path}
