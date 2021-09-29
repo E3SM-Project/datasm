82a84af39b83cd10323f44b94276d89e5b214df9
Tony Bartoletti <bartoletti1@llnl.gov>
2021-09-29 08:41:48 -0700

    warning is more appropriate here

--- warehouse/warehouse/scripts/move_to_publication.py
+++ warehouse/warehouse/scripts/move_to_publication.py
@@ -156,7 +156,7 @@ def main():
                 outstream.write(message)
                 con_message("info", message)
         else:
-            con_message("info", f"cannot obtain message_file (from message_path) from environment for message {message}")
+            con_message("warning", f"cannot obtain message_file (from message_path) from environment for message {message}")
 
     con_message("info", f"calling conduct_move with method {move_method}")
 
