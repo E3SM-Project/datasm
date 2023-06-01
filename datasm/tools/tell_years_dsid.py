438f6ce56eec22be630500751f8dd886238c5f30
Tony Bartoletti <bartoletti1@llnl.gov>
2023-06-01 11:00:18 -0700

    support alternative dataset_spec

--- datasm/tools/tell_years_dsid.py
+++ datasm/tools/tell_years_dsid.py
@@ -5,6 +5,7 @@ from argparse import RawTextHelpFormatter
 
 
 helptext = '''
+    Usage:  (python) tell_years_dsid.py -d <dataset_id> [-s <path_to_dataset_spec.yaml>]
     For the given E3SM dataset_id, report "start_year,end_year" from the dataset_spec.
 '''
 
@@ -15,13 +16,13 @@ def assess_args():
     required = parser.add_argument_group('required arguments')
     optional = parser.add_argument_group('optional arguments')
     required.add_argument('-d', '--dsid', action='store', dest="thedsid", type=str, required=True)
+    optional.add_argument('-s', '--spec', action='store', dest="thespec", type=str, required=False)
 
     args = parser.parse_args()
 
     return args
 
-# resource_path = '/p/user_pub/e3sm/staging/resource/'
-resource_path = '/home/bartoletti1/gitcode/datasm/datasm/resources'
+resource_path = '/p/user_pub/e3sm/staging/resource/'
 
 DEFAULT_SPEC_PATH = os.path.join(resource_path, 'dataset_spec.yaml')
 
@@ -31,26 +32,24 @@ def main():
     pargs = assess_args()
 
     dsid = pargs.thedsid
-    dc = dsid.split(".")
+    dc = dsid.split(".")        # E3SM:  0=project, 1=model, 2=exper, 3=resol, 4=realm, 5=
 
-    # E3SM:  0=project, 1=model, 2=exper,
-    # CMIP:  0=project, 1=activ, 2=sourc, 3=exper,
-
-    with open(DEFAULT_SPEC_PATH, 'r') as instream:
-        dataset_spec = yaml.load(instream, Loader=yaml.SafeLoader)
+    if pargs.thespec:
+        if os.path.exists(pargs.thespec):
+            with open(pargs.thespec, 'r') as instream:
+                dataset_spec = yaml.load(instream, Loader=yaml.SafeLoader)
+        else:
+            print(f"ERROR: cannot access supplied dataset_spec file: {pargs.thespec}")
+            sys.exit(1)
+        
+    else:
+        with open(DEFAULT_SPEC_PATH, 'r') as instream:
+            dataset_spec = yaml.load(instream, Loader=yaml.SafeLoader)
 
     # for experiment, experimentinfo in dataset_spec['project'][dc[0]][dc[1]].items():
     #     print(f"{experiment}: {experimentinfo}")
 
-
-    if dc[0] == "E3SM":
-        # print(f"DEBUG: headpart = {dc[0]}.{dc[1]}.{dc[2]}")
-        the_experiment_record = dataset_spec['project'][dc[0]][dc[1]][dc[2]]
-    else:
-        # print(f"DEBUG: headpart = {dc[0]}.{dc[1]}.{dc[2]}.{dc[3]}.{dc[4]}")
-        the_experiment_record = dataset_spec['project'][dc[0]][dc[1]][dc[2]][dc[3]][dc[4]]
-
-    # print(f"DEBUG: the_experiment_record = {the_experiment_record}")
+    the_experiment_record = dataset_spec['project'][dc[0]][dc[1]][dc[2]]
     print(f"{the_experiment_record['start']},{the_experiment_record['end']}")
 
 
