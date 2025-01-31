1428f6606ea4d0624ab412e248fa0c65f537c27b
Tony Bartoletti <bartoletti1@llnl.gov>
2025-01-30 16:13:06 -0800

    fix another typo in config

--- datasm/tools/local_install.sh
+++ datasm/tools/local_install.sh
@@ -8,11 +8,11 @@ tools=`$DSM_GETPATH STAGING_TOOLS`
 
 cp ../resources/dataset_spec.yaml $resource/dataset_spec.yaml
 cp ../resources/derivatives.conf $resource/derivatives.conf
-cp ../resources/Archive_Locator $$arch_man/Archive_Locator
-cp ../resources/Archive_Map $$arch_man/Archive_Map
-cp ../resources/Archive_Map_headers $$arch_man/Archive_Map_headers
-cp ../resources/NERSC_Archive_Map $$arch_man/NERSC_Archive_Map
-cp ../resources/Standard_Datatype_Extraction_Patterns $$arch_man/Standard_Datatype_Extraction_Patterns
+cp ../resources/Archive_Locator $arch_man/Archive_Locator
+cp ../resources/Archive_Map $arch_man/Archive_Map
+cp ../resources/Archive_Map_headers $arch_man/Archive_Map_headers
+cp ../resources/NERSC_Archive_Map $arch_man/NERSC_Archive_Map
+cp ../resources/Standard_Datatype_Extraction_Patterns $arch_man/Standard_Datatype_Extraction_Patterns
 
 for afile in `cat MANIFEST`; do
     cp $afile $tools
