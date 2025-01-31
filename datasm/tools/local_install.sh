13d4f0bb6859cbcf7cca438f3cd62f913e55e00d
Tony Bartoletti <bartoletti1@llnl.gov>
2025-01-30 16:08:31 -0800

    fix another path mishap in config

--- datasm/tools/local_install.sh
+++ datasm/tools/local_install.sh
@@ -3,15 +3,16 @@
 # must execute in tools directory
 
 resource=`$DSM_GETPATH STAGING_RESOURCE`
+arch_man=`$DSM_GETPATH ARCHIVE_MANAGEMENT`
 tools=`$DSM_GETPATH STAGING_TOOLS`
 
 cp ../resources/dataset_spec.yaml $resource/dataset_spec.yaml
 cp ../resources/derivatives.conf $resource/derivatives.conf
-cp ../resources/Archive_Locator $resource/archive/Archive_Locator
-cp ../resources/Archive_Map $resource/archive/Archive_Map
-cp ../resources/Archive_Map_headers $resource/archive/Archive_Map_headers
-cp ../resources/NERSC_Archive_Map $resource/archive/NERSC_Archive_Map
-cp ../resources/Standard_Datatype_Extraction_Patterns $resource/archive/Standard_Datatype_Extraction_Patterns
+cp ../resources/Archive_Locator $$arch_man/Archive_Locator
+cp ../resources/Archive_Map $$arch_man/Archive_Map
+cp ../resources/Archive_Map_headers $$arch_man/Archive_Map_headers
+cp ../resources/NERSC_Archive_Map $$arch_man/NERSC_Archive_Map
+cp ../resources/Standard_Datatype_Extraction_Patterns $$arch_man/Standard_Datatype_Extraction_Patterns
 
 for afile in `cat MANIFEST`; do
     cp $afile $tools
