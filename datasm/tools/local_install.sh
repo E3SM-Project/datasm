ff24120fb7948017f8b990c23ef6c8b522294cef
Tony Bartoletti <bartoletti1@llnl.gov>
2025-01-30 16:01:39 -0800

    fix typo in resource path

--- datasm/tools/local_install.sh
+++ datasm/tools/local_install.sh
@@ -5,13 +5,13 @@
 resource=`$DSM_GETPATH STAGING_RESOURCE`
 tools=`$DSM_GETPATH STAGING_TOOLS`
 
-cp ../resource/dataset_spec.yaml $resource/dataset_spec.yaml
-cp ../resource/derivatives.conf $resource/derivatives.conf
-cp ../resource/Archive_Locator $resource/archive/Archive_Locator
-cp ../resource/Archive_Map $resource/archive/Archive_Map
-cp ../resource/Archive_Map_headers $resource/archive/Archive_Map_headers
-cp ../resource/NERSC_Archive_Map $resource/archive/NERSC_Archive_Map
-cp ../resource/Standard_Datatype_Extraction_Patterns $resource/archive/Standard_Datatype_Extraction_Patterns
+cp ../resources/dataset_spec.yaml $resource/dataset_spec.yaml
+cp ../resources/derivatives.conf $resource/derivatives.conf
+cp ../resources/Archive_Locator $resource/archive/Archive_Locator
+cp ../resources/Archive_Map $resource/archive/Archive_Map
+cp ../resources/Archive_Map_headers $resource/archive/Archive_Map_headers
+cp ../resources/NERSC_Archive_Map $resource/archive/NERSC_Archive_Map
+cp ../resources/Standard_Datatype_Extraction_Patterns $resource/archive/Standard_Datatype_Extraction_Patterns
 
 for afile in `cat MANIFEST`; do
     cp $afile $tools
